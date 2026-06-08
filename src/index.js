6323d7cbcd867c8e0ea9d2322359da3a969c83fa
Yashwanth <yashwanth.v357edu@gmail.com>
2026-06-08 10:39:58 +0530

    feat: add error handling utilities and configure CORS support

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 import connectDB from "./db/db.js";
 import { config } from "dotenv";
+import { app } from "./app.js";
 
-dotenv.config({path:'./env'});
-
+dotenv.config({ path: "./env" });
 
 connectDB()
+  .then(() => [
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙...Server is running at port: ${process.env.PORT}`);
+    }),
+  ])
+  .catch(() => {
+    console.log("MONGODB Connection Failed !");
+  });
+
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (reqestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(reqestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = (fun) => async (req, res, next) => {
+//   try {
+//     await fun(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
+ 
